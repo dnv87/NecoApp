47d143fd9c9425bcf92d4a4129e028fc807cf1e9
PC <domashnv@mail.ru>
2022-03-06 14:39:03 +0300

    b = true

--- app/src/main/java/com/example/necoapp/MainActivity.kt
+++ app/src/main/java/com/example/necoapp/MainActivity.kt
@@ -7,7 +7,8 @@ import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var b = false
+    var b = true
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
