d4ecbb911090566b47162b0937e727925b9466be
PC <domashnv@mail.ru>
2022-03-06 11:27:45 +0300

    new button in activ

--- app/src/main/java/com/example/necoapp/MainActivity.kt
+++ app/src/main/java/com/example/necoapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.necoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    var count : Boolean = true
+    fun onClicketTst(view: View){
+        val elTetx = findViewById<TextView>(R.id.textView)
+        if (count) {
+            elTetx.text = "Abra-kodabra!!!"
+        }
+        else {
+            elTetx.text = "Shwabra-kodabra!!!"
+        }
+        count = !count
+        Log.d("MyTst", "count = ${count.toString()}")
     }
 }
